17ed28e84542253a4a634e5614d12c05b48abb8e
adfhsjt <ahmadriski207@gmail.com>
2025-02-20 11:31:48 +0700

    `Added new routes, controllers, and views for HomeController, ProductController, UserController, and SalesController`

---
+++ resources/views/products/food-beverage.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Food & Beverage</title>
+</head>
+<body>
+    <h1>Halaman Produk: Food & Beverage</h1>
+</body>
+</html>
---
+++ resources/views/products/home-care.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home Care</title>
+</head>
+<body>
+    <h1>Halaman Produk: Home Care</h1>
+</body>
+</html>
